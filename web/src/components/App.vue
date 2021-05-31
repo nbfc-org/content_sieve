93e37f1580c836e8f9d6e835626df9b81d3c9888
asdfasdf08fj938j4 <asdfasdf08fj938j4@nbfc.org>
2021-05-31 12:29:25 -0700

    get posts for web from api

--- web/src/components/App.vue
+++ web/src/components/App.vue
@@ -1,27 +1,62 @@
 <template>
   <div id="app">
-    <div class="banner">
-      <img
-        src="https://vuejs.org/images/logo.png"
-        width="100"
-        alt="vue"
-        class="logo"
-      />
-      <h1>Welcome to Vue.js</h1>
-    </div>
-    <div class="bottom">
-      To get started, edit <code>./src/components/App.vue</code> and save to reload.<br/>
-      <span class="fade">
-        Checkout <code>./README.md</code> for more usages.
-      </span>
+    <p class="username">{{ currentUser.username }}'s posts:</p>
+    <ul>
+      <li v-for="post in posts" :key="post.id">{{ post.content }}</li>
+    </ul>
+    <div>
+      <input v-model="newPostContent">
+      <button @click="addPost()">Add Post</button>
     </div>
   </div>
 </template>
 
 <script>
-  export default {
-    name: 'app'
+import gql from 'graphql-tag'
+
+const CURRENT_USER = gql`query {
+  currentUser {
+    id
+    username
   }
+}`;
+
+const POSTS_BY_USER = gql`query ($userId: String!) {
+  postsByUser(userId: $userId) {
+    id
+    content
+  }
+}`;
+
+export default {
+    name: 'app',
+    data: function(){
+        return {
+            currentUser: { username: 'user' },
+            posts: [],
+            newPostContent: ''
+
+        }
+    },
+    methods: {
+        addPost() {
+            this.posts.push({ content: this.newPostContent })
+            this.newPostContent = '';
+        }
+    },
+    apollo: {
+        currentUser: CURRENT_USER,
+        posts: {
+            query: POSTS_BY_USER,
+            variables() {
+                return { userId: this.currentUser.id }
+            },
+            update(data) {
+                return data.postsByUser
+            },
+        },
+    }
+}
 </script>
 
 <!-- CSS libraries -->
--- web/src/index.js
+++ web/src/index.js
@@ -3,7 +3,19 @@ import App from './components/App.vue'
 
 Vue.config.productionTip = false
 
+import ApolloClient from 'apollo-boost'
+import VueApollo from "vue-apollo";
+
+const apolloProvider = new VueApollo({
+  defaultClient: new ApolloClient({
+    uri: 'http://localhost:4001'
+  })
+});
+
+Vue.use(VueApollo);
+
 new Vue({
   el: '#app',
+  apolloProvider,
   render: h => h(App)
 })
