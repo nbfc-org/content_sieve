ff148a0db535392d521bcf4946d4671c5065beaa
asdfasdf08fj938j4 <asdfasdf08fj938j4@nbfc.org>
2021-07-18 22:40:09 -0700

    posts.js

--- api/server.js
+++ api/server.js
@@ -2,7 +2,7 @@ import { ApolloServer, gql } from 'apollo-server';
 
 import uuid62 from 'uuid62';
 
-import { testData } from './posts.mjs';
+import { testData } from './posts.js';
 
 const schema = gql(`
   type Query {
