834415fc1e664a4714a0a5cfb66bfdbadcc82c1d
asdfasdf08fj938j4 <asdfasdf08fj938j4@nbfc.org>
2021-08-07 18:33:35 -0700

    rm dead code from posts.js

--- web/src/lib/posts.js
+++ web/src/lib/posts.js
@@ -1,34 +1,6 @@
 import SymbolTree from 'symbol-tree';
 import uuid62 from 'uuid62';
 
-const p = [
-    {
-        id: uuid62.v4(),
-        content: {
-            body: "First Post - Hello world",
-        },
-        createdAt: Date.now(),
-        userId: 'abc-1',
-    },
-    {
-        id: uuid62.v4(),
-        content: {
-            body: "Second Post - Hello again",
-        },
-        createdAt: Date.now(),
-        userId: 'abc-1',
-    },
-    {
-        id: uuid62.v4(),
-        content: {
-            title: "Random Post",
-            url: "https://google.com",
-        },
-        createdAt: Date.now(),
-        userId: 'abc-1',
-    }
-];
-
 class Thread {
     constructor(newPost) {
         this.tree = new SymbolTree();
@@ -60,43 +32,7 @@ class Thread {
             const parent = this.lookup[parentId];
             this.tree.prependChild(parent, newPost);
         }
-      /*
-        const pathmap = {};
-        pathmap[id] = [...index, 0];
-        return this._objectWithContext(newPost, pathmap);
-        */
-    }
-    _objectWithContext(o, pathmap) {
-        const p = this.tree.parent(o);
-        if (p) {
-            return Object.assign({}, o, { parent: p.id, index: pathmap[o.id] });
-        }
-        return o;
     }
 }
 
-const testData = function() {
-    const thread = new Thread();
-    thread.reply(undefined, p[0]);
-    for (const i of [...Array(20).keys()]) {
-        thread.reply(p[0].id, Object.assign({}, p[1], {id: uuid62.v4()}));
-        thread.reply(p[0].id, Object.assign({}, p[2], {id: uuid62.v4()}));
-    }
-    const data = {};
-
-    data.thread = thread;
-
-    data.users = [
-        {
-            id: 'abc-1',
-            username: "andy25",
-        },
-        {
-            id: 'abc-2',
-            username: "randomUser",
-        }
-    ];
-    return data;
-};
-
-export { testData, Thread };
+export { Thread };
