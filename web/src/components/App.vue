ac0f01d9852187ba89f0aa0599099f62de0a9b80
asdfasdf08fj938j4 <asdfasdf08fj938j4@nbfc.org>
2021-08-29 10:22:01 -0700

    use vuetify nav

--- web/src/components/App.vue
+++ web/src/components/App.vue
@@ -1,8 +1,6 @@
 <template>
   <v-app>
-    <v-app-bar app>
-      <app-nav></app-nav>
-    </v-app-bar>
+    <app-nav></app-nav>
     <v-main>
       <v-container fluid>
         <router-view></router-view>
