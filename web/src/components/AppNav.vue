0583ad1d60b8094127edb73cec38296e220bfbb6
asdfasdf08fj938j4 <asdfasdf08fj938j4@nbfc.org>
2021-09-06 09:12:21 -0700

    make app nav smaller

--- web/src/components/AppNav.vue
+++ web/src/components/AppNav.vue
@@ -1,13 +1,13 @@
 <template>
-  <v-app-bar app>
+  <v-app-bar app dense>
     <v-toolbar-title>
-      content sieve
+      <v-btn to="/" exact>content sieve</v-btn>
     </v-toolbar-title>
     <v-spacer/>
     <v-toolbar-items>
-      <v-btn to="/tag/all" exact>posts</v-btn>
-      <v-btn to="/tree" exact>full tree</v-btn>
-      <v-btn to="/new" exact>new</v-btn>
+      <v-btn to="/tag/all" small exact>posts</v-btn>
+      <v-btn to="/tree" small exact>full tree</v-btn>
+      <v-btn to="/new" small exact>new</v-btn>
     </v-toolbar-items>
   </v-app-bar>
 </template>
