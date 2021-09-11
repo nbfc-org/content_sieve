55220540ee39f308f6dc88495a374a6c9886e592
asdfasdf08fj938j4 <asdfasdf08fj938j4@nbfc.org>
2021-09-10 19:52:40 -0700

    move log in/out into nav bar

--- web/src/components/App.vue
+++ web/src/components/App.vue
@@ -7,12 +7,6 @@
       </v-container>
       <v-card>
         <!-- Check that the SDK client is not currently loading before accessing is methods -->
-        <div v-if="!$auth.loading">
-          <!-- show login when not authenticated -->
-          <button v-if="!$auth.isAuthenticated" @click="login">Log in</button>
-          <!-- show logout when authenticated -->
-          <button v-if="$auth.isAuthenticated" @click="logout">Log out</button>
-        </div>
       </v-card>
     </v-main>
   </v-app>
@@ -24,17 +18,6 @@ import AppNav from './AppNav.vue';
 export default {
     components: {
         AppNav,
-    },
-    methods: {
-        login() {
-            this.$auth.loginWithRedirect();
-        },
-        // Log the user out
-        logout() {
-            this.$auth.logout({
-                returnTo: window.location.origin
-            });
-        },
     },
 }
 </script>
--- web/src/components/AppNav.vue
+++ web/src/components/AppNav.vue
@@ -5,13 +5,25 @@
     </v-toolbar-title>
     <v-spacer/>
     <v-toolbar-items>
-      <v-btn to="/tag/all" small exact>posts</v-btn>
       <v-btn to="/tree" small exact>full tree</v-btn>
-      <v-btn to="/new" small exact>new</v-btn>
+      <v-btn v-if="$auth.isAuthenticated" to="/new" small exact>new post</v-btn>
+      <v-btn v-if="!$auth.loading && $auth.isAuthenticated" small exact @click="logout">Log out</v-btn>
+      <v-btn v-if="!$auth.loading && !$auth.isAuthenticated" small exact @click="login">Log in</v-btn>
     </v-toolbar-items>
   </v-app-bar>
 </template>
 
 <script>
-export default {}
+export default {
+    methods: {
+        login() {
+            this.$auth.loginWithRedirect();
+        },
+        logout() {
+            this.$auth.logout({
+                returnTo: window.location.origin
+            });
+        },
+    },
+}
 </script>
