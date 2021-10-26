049a603d7bd3639144378705a032ff35258e3ab0
asdfasdf08fj938j4 <asdfasdf08fj938j4@nbfc.org>
2021-10-25 20:21:03 -0700

    add responsive nav bar drawer

--- TODO.md
+++ TODO.md
@@ -3,9 +3,6 @@
 - via css, make top level post obvious
 - https://old.reddit.com/r/gamedev/comments/qeqn3b/despite_having_just_58_sales_over_38_of_bug/
 
-
-- drawer in nav bar
-
 - #anchor to parent if post is on page
 
 - get rid of self-votes / surface votes to client
--- web/src/components/AppNav.vue
+++ web/src/components/AppNav.vue
@@ -1,22 +1,82 @@
 <template>
-  <v-app-bar app dense>
-    <v-toolbar-title>
-      <v-btn to="/" exact>{{ title }}</v-btn>
-    </v-toolbar-title>
-    <v-spacer/>
-    <v-toolbar-items>
-      <v-btn v-if="authed()" to="/new" x-small exact>New Post</v-btn>
-      <v-btn v-if="isAdmin()" to="/user/metavote" x-small exact>Meta Vote</v-btn>
-      <v-btn to="/user/settings" x-small exact>Settings</v-btn>
-      <v-btn v-if="authed()" x-small exact @click="logout">Log out</v-btn>
-      <v-btn v-if="!authed()" x-small exact @click="login">Log in</v-btn>
-    </v-toolbar-items>
-  </v-app-bar>
+  <v-container>
+    <v-navigation-drawer v-model="sidebar" app>
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="text-h6">
+            {{ title }}
+          </v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list dense nav>
+        <v-list-item v-for="item in menuItems" v-if="item.test()" :key="item.title" :to="item.path">
+          <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>{{ item.title }}</v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar app dense>
+      <span class="hidden-sm-and-up">
+        <v-app-bar-nav-icon @click="sidebar = !sidebar"></v-app-bar-nav-icon>
+      </span>
+      <v-toolbar-title>
+        <v-btn to="/" exact>{{ title }}</v-btn>
+      </v-toolbar-title>
+      <v-spacer/>
+      <v-toolbar-items class="hidden-xs-only">
+        <v-btn x-small v-for="item in menuItems" v-if="item.test()" :key="item.title" :to="item.path">
+          {{ item.title }}
+        </v-btn>
+      </v-toolbar-items>
+      <v-toolbar-items>
+        <v-btn v-if="authed()" x-small @click="logout">
+          Log out
+        </v-btn>
+        <v-btn v-if="!authed()" x-small @click="login">
+          Log in
+        </v-btn>
+      </v-toolbar-items>
+    </v-app-bar>
+  </v-container>
 </template>
 
 <script>
 import pkg from '../../package.json';
+
+import { mdiAccountCog, mdiBallot, mdiNotePlus } from '@mdi/js';
+import { mdiLoginVariant, mdiLogoutVariant } from '@mdi/js';
+
 export default {
+    data: function() {
+        return {
+            mdiLogoutVariant,
+            mdiLoginVariant,
+            menuItems: [
+                { title: "New Post",
+                  path: "/new",
+                  icon: mdiNotePlus,
+                  test: this.authed,
+                },
+                { title: "Meta Vote",
+                  path: "/user/metavote",
+                  icon: mdiBallot,
+                  test: this.isAdmin,
+                },
+                { title: "Settings",
+                  path: "/user/settings",
+                  icon: mdiAccountCog,
+                  test: () => true,
+                },
+            ],
+            sidebar: false,
+        };
+    },
     computed: {
         title() {
             return pkg.productName;
