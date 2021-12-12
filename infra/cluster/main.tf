76e7117d1eaa46f2a50b07d46125abd3e186a181
asdfasdf08fj938j4 <asdfasdf08fj938j4@nbfc.org>
2021-12-11 19:15:24 -0800

    add docker droplet

--- infra/cluster/main.tf
+++ infra/cluster/main.tf
@@ -8,8 +8,27 @@ data "digitalocean_project" "project" {
   name="nbfc ${terraform.workspace}"
 }
 
+resource "digitalocean_project_resources" "resources" {
+  project = data.digitalocean_project.project.id
+
+  # Bucket Database Domain DomainRecord Droplet Firewall FloatingIp Image Kubernetes LoadBalancer MarketplaceApp Saas Volume
+  resources = [
+    digitalocean_droplet.docker.urn,
+  ]
+}
+
 resource "digitalocean_vpc" "vpc" {
   name   = "sfo3-vpc-01"
   region = "sfo3"
   ip_range = "10.124.0.0/20"
+}
+
+resource "digitalocean_droplet" "docker" {
+  image  = "debian-11-x64"
+  name   = "debian-s-1vcpu-1gb-amd-sfo3-01"
+  region = "sfo3"
+  size   = "s-1vcpu-1gb-amd"
+  tags = [
+    terraform.workspace,
+  ]
 }
