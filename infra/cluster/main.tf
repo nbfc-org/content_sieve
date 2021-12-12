228d191171e788cc2a785e8b924fd808d6af236c
asdfasdf08fj938j4 <asdfasdf08fj938j4@nbfc.org>
2021-12-11 19:05:51 -0800

    add vpc

--- infra/cluster/main.tf
+++ infra/cluster/main.tf
@@ -3,3 +3,13 @@ resource "digitalocean_project" "project" {
   environment=terraform.workspace
   purpose="Just trying out DigitalOcean"
 }
+
+data "digitalocean_project" "project" {
+  name="nbfc ${terraform.workspace}"
+}
+
+resource "digitalocean_vpc" "vpc" {
+  name   = "sfo3-vpc-01"
+  region = "sfo3"
+  ip_range = "10.124.0.0/20"
+}
