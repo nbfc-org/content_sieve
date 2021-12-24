7993a18d75c584fda028de425269628f3eae616a
asdfasdf08fj938j4 <asdfasdf08fj938j4@nbfc.org>
2021-12-24 08:54:08 -0800

    WIP: import create app

--- api/src/ui.ts
+++ api/src/ui.ts
@@ -1,5 +1,12 @@
 import * as ApolloSSR from '@vue/apollo-ssr';
+// import createApp from './app';
 
 export function ui(req, res) {
+    /*
+    const { app, router, store, apolloProvider } = createApp({
+        ssr: true,
+    });
+    */
+
     res.status(500).end('Internal Server Error');
 }
