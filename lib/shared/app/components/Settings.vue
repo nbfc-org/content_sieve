cf5ee1c16df72544186c7dcb9a57e746662b2d32
asdfasdf08fj938j4 <asdfasdf08fj938j4@nbfc.org>
2021-12-25 17:50:16 -0800

    massage settings page

--- lib/shared/app/components/Settings.vue
+++ lib/shared/app/components/Settings.vue
@@ -1,22 +1,25 @@
 <template>
-  <v-card>
+  <v-card class="d-flex flex-row">
     <v-card-text>
-      <h3>Settings</h3>
+      <h3>User Page</h3>
       <v-divider class="mb-3 mt-3" />
-      <h5>Sort Order for Posts & Comments</h5>
-      <v-radio-group mandatory v-model="sortType">
-        <v-radio :key="`sortTypeBtn_${s}`" v-for="s in sortTypes" :value="s" :label="s" />
-      </v-radio-group>
-      <h5>Comment Settings</h5>
-      <v-switch v-model="nested" inset label="Nested?" />
-    </v-card-text>
-    <v-card-actions>
-      <v-item-group>
-        <v-btn color="primary" outlined v-if="authed()" @click="logout">
+      <v-card flat>
+        <v-card-text>
+          <h5>Sort Order for Posts & Comments</h5>
+          <v-radio-group mandatory v-model="sortType">
+            <v-radio :key="`sortTypeBtn_${s}`" v-for="s in sortTypes" :value="s" :label="s" />
+          </v-radio-group>
+          <h5>Comment Settings</h5>
+          <v-switch v-model="nested" inset label="Nested?" />
+        </v-card-text>
+      </v-card>
+      <v-divider class="mb-3 mt-3" />
+      <v-col class="text-right">
+        <v-btn color="primary" small outlined v-if="authed()" @click="logout">
           Log out
         </v-btn>
-      </v-item-group>
-    </v-card-actions>
+      </v-col>
+    </v-card-text>
   </v-card>
 </template>
 <script>
